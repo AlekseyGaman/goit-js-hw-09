8fadf93ebc7091d4d5872ad2d22e206a6360e8fa
AlekseyGaman <4117988@gmail.com>
2022-10-07 23:52:27 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    getRandomHexColor();
+  }, 1000);
+  if ((startBtn.disabled = true)) {
+    stopBtn.disabled = false;
+  }
+  console.log(`Start - !`);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  if ((stopBtn.disabled = true)) {
+    startBtn.disabled = false;
+  }
+  console.log(`Stop!`);
+});
